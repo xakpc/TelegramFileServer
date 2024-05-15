9bac5d0833e2a008ac57826f91b1e14ad10cf936
Pavel Osadchuk <xakz.pc@gmail.com>
2024-05-15 20:09:08 +0200

    chore: expand description

--- Xakpc.TelegramFileServer/Program.cs
+++ Xakpc.TelegramFileServer/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.OpenApi.Models;
 using Xakpc.TelegramFileServer.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,7 +11,21 @@ builder.Services.AddRouting(options => options.LowercaseUrls = true);
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Telegram File Server API",
+        Version = "v1",
+        Description = "REST-based file server API that uses Telegram Bot as storage. " +
+                      "[How to use in the README](https://github.com/xakpc/TelegramFileServer?tab=readme-ov-file#how-to-use).",
+        Contact = new OpenApiContact
+        {
+            Name = "@xakpc",
+            Url = new Uri("https://x.com/xakpc")
+        }
+    });
+});
 builder.Services.AddProblemDetails();
 builder.Services.AddMemoryCache();
 builder.Services.AddHttpClient();
