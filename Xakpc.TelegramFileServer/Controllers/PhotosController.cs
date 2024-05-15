68908047685fe259c0f940cc4cf7e39b00b9e37b
Pavel Osadchuk <xakz.pc@gmail.com>
2024-05-15 18:13:35 +0200

    init: initial commit

---
+++ Xakpc.TelegramFileServer/Controllers/PhotosController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Xakpc.TelegramFileServer.Services;
+
+namespace Xakpc.TelegramFileServer.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PhotosController : ControllerBase
+{
+    private readonly IStorageService _storageService;
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public PhotosController(IHttpClientFactory httpClientFactory, IStorageService storageService)
+    {
+        _storageService = storageService;
+        _httpClientFactory = httpClientFactory;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> UploadImage(IFormFile photo,
+        [FromHeader(Name = "Chat-Id")] string chatId,
+        [FromHeader(Name = "Bot-Token")] string botToken,        
+        CancellationToken cancellationToken)
+    {
+        if (photo == null || photo.Length == 0)
+            return BadRequest("No image uploaded.");
+
+        var fileId = await _storageService.UploadImageAsync(
+            new TelegramCredentials(botToken, chatId),
+            photo, cancellationToken);
+
+        return Created($"api/v1/photos/{fileId}", fileId);
+    }
+
+    [HttpGet("{fileId}")]
+    public async Task<IActionResult> DownloadImage(string fileId, 
+        [FromHeader(Name = "Bot-Token")] string botToken)
+    {
+        var imageUrl = await _storageService.GetImageUrlAsync(
+            new TelegramCredentials(botToken, default), fileId);
+
+        if (imageUrl == null)
+            return NotFound();
+
+        var httpClient = _httpClientFactory.CreateClient();
+        var response = await httpClient.GetAsync(imageUrl);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            return StatusCode((int)response.StatusCode);
+        }
+
+        var contentType = GetContentType(Path.GetExtension(imageUrl));
+        var imageStream = await response.Content.ReadAsStreamAsync();
+        return File(imageStream, contentType);
+    }
+
+    private string GetContentType(string extension)
+    {
+        return extension.ToLower() switch
+        {
+            ".jpg" => "image/jpeg",
+            ".jpeg" => "image/jpeg",
+            ".png" => "image/png",
+            ".gif" => "image/gif",
+            ".bmp" => "image/bmp",
+            ".tiff" => "image/tiff",
+            ".svg" => "image/svg+xml",
+
+            ".webp" => "image/webp",
+            ".mp4" => "video/mp4",
+            ".webm" => "video/webm",
+            ".ogg" => "video/ogg",
+
+            ".pdf" => "application/pdf",
+            ".doc" => "application/msword",
+            ".docx" => "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+            ".xls" => "application/vnd.ms-excel",
+            ".xlsx" => "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+            ".ppt" => "application/vnd.ms-powerpoint",
+            _ => "application/octet-stream",
+        };
+    }
+}
+
---
+++ Xakpc.TelegramFileServer/Services/TelegramStorageService.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace Xakpc.TelegramFileServer.Services;
+
+public interface IStorageService
+{
+    Task<Document> UploadImageAsync(TelegramCredentials credentials, IFormFile image, CancellationToken cancellationToken);
+    Task<string?> GetImageUrlAsync(TelegramCredentials credentials, string fileId);
+}
+
+public record TelegramCredentials(string BotToken, string? ChatId);
+
+public class TelegramStorageService : IStorageService
+{
+    private IMemoryCache _memoryCache;
+
+    public TelegramStorageService(IMemoryCache memoryCache)
+    {
+        _memoryCache = memoryCache;
+    }
+
+    public async Task<Document> UploadImageAsync(TelegramCredentials credentials, IFormFile image, CancellationToken cancellationToken)
+    {
+        var botClient = new TelegramBotClient(credentials.BotToken);
+
+        await using var imageStream = image.OpenReadStream();
+
+        // Send the photo using the bot client
+        var message = await botClient.SendDocumentAsync(
+            chatId: credentials.ChatId,
+            document: InputFile.FromStream(imageStream, image.FileName),
+            disableNotification: true,           
+            cancellationToken: cancellationToken);
+
+        return message.Document;
+    }
+
+    public async Task<string?> GetImageUrlAsync(TelegramCredentials credentials, string fileId)
+    {
+        var metadata = await GetMetadataAsync(credentials, fileId);
+
+        if (metadata == null)
+        {
+            return null;
+        }
+
+        return $"https://api.telegram.org/file/bot{credentials.BotToken}/{metadata.FilePath}";
+    }
+
+    private async Task<Telegram.Bot.Types.File?> GetMetadataAsync(TelegramCredentials credentials, string fileId)
+    {
+        var botClient = new TelegramBotClient(credentials.BotToken);
+
+        // add memory cache to avoid multiple requests to the Telegram API
+        if (_memoryCache.TryGetValue(fileId, out Telegram.Bot.Types.File file))
+        {
+            return file;
+        }
+
+        var fileInfo = await botClient.GetFileAsync(fileId);
+        
+        if (fileInfo != null)
+        {
+            _memoryCache.Set(fileId, fileInfo, TimeSpan.FromHours(1));
+        }
+        
+        return fileInfo;
+    }
+}
